719b904dd83b284b4a3d754c0b1125439c9bfc2c
scottmotte <scott@scottmotte.com>
2013-11-29 12:16:33 -0800

    Update click-to-cloud to a command line binary

---
+++ README.md
@@ -0,0 +1,28 @@
+# click-to-cloud
+
+Make it possible to deploy applications to heroku and other places with just a click. 
+
+## Usage
+
+```bash
+click-to-cloud --repo https://github.com/scottmotte/handshake.git
+```
+
+Replace the repo with the repo you want to deploy. It needs to be a click-to-cloud enabled repo. It it is
+it will have a a `click-to-cloud.json` file in the root of its project directory.
+
+## How does it work
+
+The developer defines a set of deployment rules in their application inside a click-to-cloud.json file. 
+The click-to-cloud script takes care of the rest - knowing only the location of the git repo.
+
+## Installation
+
+You need to install the click-to-cloud binary on your machine. Do the following to do so. You must have go.
+
+```bash
+git clone https://github.com/scottmotte/click-to-cloud.git
+cd click-to-cloud
+go install
+```
+
--- click-to-cloud.go
+++ click-to-cloud.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  cli "github.com/codegangsta/cli"
   "fmt"
   "io/ioutil"
   "log"
@@ -11,52 +12,67 @@ import (
 )
 
 func main() {
-  os.MkdirAll("deploys", 0777)
-  os.Chdir("deploys")
+  app       := cli.NewApp()
+  app.Name  = "click-to-cloud"
+  app.Usage = "Deploy applications to the cloud with just a 'click'."
 
-  repo_url        := "https://github.com/scottmotte/handshake.git"
-  repo_url_split  := strings.Split(repo_url, "/")
-  repo_url_split_length := len(repo_url_split)
-  repo_name       := strings.Split(repo_url_split[repo_url_split_length-1], ".git")[0]
-
-  // Step 1: Clone repo 
-  clone_cmd         := exec.Command("git", "clone", repo_url)
-  clone_cmd.Stdout  = os.Stdout
-  clone_cmd.Stderr  = os.Stderr
-  err               := clone_cmd.Run()
-  if err != nil {
-    log.Fatal(err)
+  app.Flags = []cli.Flag {
+    cli.StringFlag{"repo", "", "Git url repository of click-to-cloud enabled app"},
   }
 
-  // Step 2: chdir
-  os.Chdir(repo_name)
+  app.Action = func(c *cli.Context) {
+    os.MkdirAll("deploys", 0777)
+    os.Chdir("deploys")
 
-  // Step 3: Read and parse click-to-cloud.json
-  data, err := ioutil.ReadFile("./click-to-cloud.json")
-  json, err := simplejson.NewJson(data)
-  if err != nil {
-    log.Fatal(err)
-  }
+    repo_url        := c.String("repo")
+    repo_url_split  := strings.Split(repo_url, "/")
+    repo_url_split_length := len(repo_url_split)
+    repo_name       := strings.Split(repo_url_split[repo_url_split_length-1], ".git")[0]
 
-  // Step 4: run commands
-  commands, _ := json.Get("heroku").Array()
-
-  for _, command := range commands {
-    command_to_string, _ := command.(string)
-    fmt.Println(command_to_string)
-    fmt.Println("Running command: "+command_to_string)
-
-    command_slice           := strings.Split(command_to_string, " ")
-    command_slice_end_range := len(command_slice)
-
-    exec_command        := exec.Command(command_slice[0], command_slice[1:command_slice_end_range] ...)
-    exec_command.Stdout = os.Stdout
-    exec_command.Stderr = os.Stderr
-    err                 := exec_command.Run()
+    // Step 1: Clone repo 
+    clone_cmd         := exec.Command("git", "clone", repo_url)
+    clone_cmd.Stdout  = os.Stdout
+    clone_cmd.Stderr  = os.Stderr
+    err               := clone_cmd.Run()
     if err != nil {
       log.Fatal(err)
     }
+
+    // Step 2: chdir
+    os.Chdir(repo_name)
+
+    // Step 3: Read and parse click-to-cloud.json
+    data, err := ioutil.ReadFile("./click-to-cloud.json")
+    json, err := simplejson.NewJson(data)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // Step 4: run commands
+    commands, _ := json.Get("heroku").Array()
+
+    for _, command := range commands {
+      command_to_string, _ := command.(string)
+      fmt.Println(command_to_string)
+      fmt.Println("Running command: "+command_to_string)
+
+      command_slice           := strings.Split(command_to_string, " ")
+      command_slice_end_range := len(command_slice)
+
+      exec_command        := exec.Command(command_slice[0], command_slice[1:command_slice_end_range] ...)
+      exec_command.Stdout = os.Stdout
+      exec_command.Stderr = os.Stderr
+      err                 := exec_command.Run()
+      if err != nil {
+        log.Fatal(err)
+      }
+    }
+
+    // Step 5: cleanup
+    os.Remove(repo_name)
   }
+
+  app.Run(os.Args)
 }
 
 
